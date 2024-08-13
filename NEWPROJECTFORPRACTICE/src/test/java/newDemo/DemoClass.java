00b018f2e97900f9b8d3d175181c6a7b456f58f7
rahul-cyber-1 <rahulkhrt123@gmail.com>
2024-08-13 16:20:04 +0530

    newframeworkcreated

---
+++ NEWPROJECTFORPRACTICE/src/test/java/newDemo/DemoClass.java
@@ -0,0 +1,15 @@
+package newDemo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		
+     List<Integer> lst=new ArrayList<>();
+     
+	}
+	
+}
